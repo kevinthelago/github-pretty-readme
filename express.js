00d55521c790c703f421a2d5f15254daa0c7225b
Kevin Lago <kevinthelago@gmail.com>
2025-03-12 18:25:15 -0400

    PORT

--- express.js
+++ express.js
@@ -4,7 +4,25 @@ import express from 'express';
 
 // const express = require('express');
 const app = express();
-app.listen(process.env.PORT || 8080);
+var port = normalizePort(process.env.PORT || 8080);
+app.set("port", 8080)
+// app.listen(process.env.PORT || 8080);
 
 app.get('/account-summary', accountSummary);
 // app.get('/repository-readme', repositoryReadme);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
