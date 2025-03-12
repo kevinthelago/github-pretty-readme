9e835a140e8bd8dae7abc0a7170771e3f66643de
Kevin Lago <kevinthelago@gmail.com>
2025-03-12 18:36:02 -0400

    yerp removed return

--- express.js
+++ express.js
@@ -13,9 +13,9 @@ app.get('/account-summary', accountSummary);
 
 app.use(function (req, res, next) {
     next(createError(404));
-  });
+});
 
-  app.use(function (err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -23,10 +23,7 @@ app.use(function (req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render("error");
-  });
-
-  return app;
-}
+});
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
