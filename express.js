49f369c81489cb2d52b9cda98d06d7c98e7ac45f
Kevin Lago <kevinthelago@gmail.com>
2025-03-12 18:41:02 -0400

    gave up

--- express.js
+++ express.js
@@ -1,29 +1,67 @@
-import accountSummary from './api/account-summary.js';
-// import repositoryReadme from './api/repository-readme.js';
-import express from 'express';
+var createError = require("http-errors");
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { format } = require("date-fns");
 
-// const express = require('express');
-const app = express();
-var port = normalizePort(process.env.PORT || '8080');
-app.set("port", port)
-// app.listen(process.env.PORT || 8080);
+// 1st party dependencies
+var indexRouter = require("./routes/index");
 
-app.get('/account-summary', accountSummary);
-// app.get('/repository-readme', repositoryReadme);
+async function getApp() {
 
-app.use(function (req, res, next) {
-    next(createError(404));
-});
+    // Database
+    // Use AZURE_COSMOS_CONNECTIONSTRING if available, otherwise fall back to MONGODB_URI
+    const mongoUri = process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.MONGODB_URI;
 
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+    mongoose.connect(mongoUri).then(() => {
+        console.log('Connected to database');
+    }).catch((err) => {
+        console.error('Error connecting to database:', err);
+    });
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-});
+    var app = express();
+
+    var port = normalizePort(process.env.PORT || '3000');
+    app.set('port', port);
+
+    // view engine setup
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "pug");
+
+    app.use(logger("dev"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, "public")));
+
+    app.locals.format = format;
+
+    app.get('/account-summary', accountSummary);
+    // app.get('/repository-readme', repositoryReadme);
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        next(createError(404));
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get("env") === "development" ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.render("error");
+    });
+
+    return app;
+}
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
@@ -40,3 +78,6 @@ function normalizePort(val) {
 
     return false;
 }
+module.exports = {
+    getApp
+};
