e8615c9f2d3fd38ea9d3b77150c6c96dedf0add2
Kevin Lago <kevinthelago@gmail.com>
2025-03-12 18:49:13 -0400

    lel am ded

--- express.js
+++ express.js
@@ -1,83 +1,10 @@
-import createError from "http-errors";
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
+import accountSummary from './api/account-summary.js';
+// import repositoryReadme from './api/repository-readme.js';
+import express from 'express';
 
-// var logger = require("morgan");
-// const { format } = require("date-fns");
+// const express = require('express');
+const app = express();
+app.listen(process.env.port || 8080);
 
-// 1st party dependencies
-var indexRouter = require("./routes/index");
-
-async function getApp() {
-
-    // Database
-    // Use AZURE_COSMOS_CONNECTIONSTRING if available, otherwise fall back to MONGODB_URI
-    const mongoUri = process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.MONGODB_URI;
-
-    mongoose.connect(mongoUri).then(() => {
-        console.log('Connected to database');
-    }).catch((err) => {
-        console.error('Error connecting to database:', err);
-    });
-
-    var app = express();
-
-    var port = normalizePort(process.env.PORT || '3000');
-    app.set('port', port);
-
-    // view engine setup
-    app.set("views", path.join(__dirname, "views"));
-    app.set("view engine", "pug");
-
-    app.use(logger("dev"));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, "public")));
-
-    app.locals.format = format;
-
-    app.get('/account-summary', accountSummary);
-    // app.get('/repository-readme', repositoryReadme);
-
-    // catch 404 and forward to error handler
-    app.use(function (req, res, next) {
-        next(createError(404));
-    });
-
-    // error handler
-    app.use(function (err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get("env") === "development" ? err : {};
-
-        // render the error page
-        res.status(err.status || 500);
-        res.render("error");
-    });
-
-    return app;
-}
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-    var port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
-}
-module.exports = {
-    getApp
-};
+app.get('/account-summary', accountSummary);
+// app.get('/repository-readme', repositoryReadme);
