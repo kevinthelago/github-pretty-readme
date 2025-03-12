ab5ae2b0effbb1bf4af9760b9eb08564a6f1c531
Kevin Lago <kevinthelago@gmail.com>
2025-03-12 19:58:59 -0400

    Changed env Variable Name

--- src/ai/model.js
+++ src/ai/model.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const googleAIStudioKey = process.env.GOOGLE_AI_STUDIO_KEY;
-const prompt = process.env.PROMPT;
+const prompt = process.env.AI_PROMPT;
 const genAI = new GoogleGenerativeAI(googleAIStudioKey);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
